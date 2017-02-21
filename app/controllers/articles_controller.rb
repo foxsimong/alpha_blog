c5eea9be7c49464fc36ed346cbce63c456d4293b
Simon <foxsimong@gmail.com>
2017-02-21 17:05:28 -0500

    pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first 
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
